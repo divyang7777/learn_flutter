f8eb275f9ab301ada15582772bf39749a31de7f5
Divyang Mistry <divyangmistry7777@gmail.com>
2018-12-26 18:56:28 +0530

    created the make it rain app

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,57 +1,146 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class MakeItRain extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new makeItRainState();
+  }
+}
+
+class makeItRainState extends State<MakeItRain> {
+  int _moneyCounter = 0;
+
+  void _rainMoney() {
+    setState(() {
+      _moneyCounter = _moneyCounter + 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Container(
-      color: Colors.amberAccent,
-      alignment: Alignment.center,
-      child: new Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Text(
-            'Item One',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 12.9),
-          ),
-          new Text(
-            'Item Two',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 12.9),
-          ),
-          const Expanded(child: const Text('Dee_BUG'))
-//          new Text(
-//            'Item Three',
-//            textDirection: TextDirection.ltr,
-//            style: TextStyle(fontSize: 12.9),
-//          ),
-        ],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Make It Rain!'),
+        backgroundColor: Colors.lightGreen,
       ),
-//      child: new Column(
-//        mainAxisAlignment: MainAxisAlignment.center,
-//        children: <Widget>[
-//          new Text(
-//            'First Item',
-//            textDirection: TextDirection.ltr,
-//            style: TextStyle(color: Colors.blueAccent),
-//          ),
-//          new Text(
-//            'Second Item',
-//            textDirection: TextDirection.ltr,
-//            style: TextStyle(color: Colors.redAccent),
-//          ),
-//          new Container(
-//            color: Colors.yellowAccent,
-//            alignment: Alignment.center,
-//            child: new Text(
-//              'Third Item',
-//              textDirection: TextDirection.ltr,
-//              style: TextStyle(color: Colors.greenAccent),
-//            ),
-//          )
-//        ],
-//      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Center(
+                child: new Text(
+              'Get Rich!',
+              style: new TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 29.9),
+            )),
+            new Expanded(
+              child: new Center(
+                child: new Text('\$$_moneyCounter',
+                  style: new TextStyle(
+                      color: _moneyCounter > 5000 ? Colors.green: Colors.red,
+                      fontSize: 46.9,
+                      fontWeight: FontWeight.w800),
+                ),
+              ),
+            ),
+            new Expanded(
+              child: new Center(
+                child: new FlatButton(
+                  color: Colors.lightGreen,
+                  textColor: Colors.white70,
+                  onPressed: () => _rainMoney(),
+                  child: new Text(
+                    'Let It Rain!',
+                    style: new TextStyle(
+                      fontSize: 19.9,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
+
+// class Home extends StatelessWidget {
+//   final String title;
+
+//   Home({Key key, this.title}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return new Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.green,
+//           title: new Text(title),
+//         ),
+//         body: new Container(
+//           alignment: Alignment.topCenter,
+//           child: new Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: <Widget>[
+//               new Text(
+//                 'Get Rich!',
+//                 textDirection: TextDirection.ltr,
+//                 style: new TextStyle(
+//                     fontSize: 38.5,
+//                     fontWeight: FontWeight.w400,
+//                     color: Colors.grey),
+//               ),
+//               new Padding(
+//                 padding: EdgeInsetsDirectional.only(top: 160.0),
+//                 child: new Text(
+//                   '\$1400',
+//                   style: new TextStyle(
+//                       fontSize: 80.0,
+//                       fontWeight: FontWeight.w900,
+//                       color: Colors.lightGreen),
+//                 ),
+//               ),
+//               new Padding(
+//                 padding: EdgeInsetsDirectional.only(top: 200.0),
+//                 child: new CustomBotton(),
+//               )
+//             ],
+//           ),
+//         ));
+//   }
+// }
+
+// class CustomBotton extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return new GestureDetector(
+//       onTap: () {
+//         final snackBar = new SnackBar(
+//           backgroundColor: Theme.of(context).backgroundColor,
+//           content: new Text("Hello Gesture"),
+//           duration: new Duration(
+//               hours: 0,
+//               minutes: 0,
+//               seconds: 0,
+//               milliseconds: 4100,
+//               microseconds: 0),
+//         );
+
+//         Scaffold.of(context).showSnackBar(snackBar);
+//       },
+//       child: new Container(
+//         padding: new EdgeInsets.all(15.0),
+//         decoration: new BoxDecoration(
+//           color: Colors.green.shade400,
+//           borderRadius: new BorderRadius.circular(5.5),
+//         ),
+//         child: new Text('Let It Rain!',textDirection: TextDirection.ltr,style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.w500,color: Colors.white),),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import './UI/home.dart';
 
 void main() {
+  var title = 'Make It Rain!';
   runApp(new MaterialApp(
-    title: "my APP",
-    home:Home(),
+    title: title,
+    home: new MakeItRain(),
+    // home:Home(title: title,),
   ));
 }
